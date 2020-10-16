21af62cc0a00a989c161100e86f0d003c459f5ce
KacperMazurIT <kacper.mazur.it@gmail.com>
2020-10-16 12:41:41 +0200

    Created Rental Movie project.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace MovieRental.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "Your application description page!";
 
             return View();
         }
